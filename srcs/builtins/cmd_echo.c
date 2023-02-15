b14675a512430c145092293b6bce8802931743a3
Jules MUNTZ-BERGER <julmuntz@student.42.fr>
2023-02-15 18:04:19 +0100

    echo FIXED

--- srcs/builtins/cmd_echo.c
+++ srcs/builtins/cmd_echo.c
@@ -6,32 +6,53 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 11:11:26 by julmuntz          #+#    #+#             */
-/*   Updated: 2023/02/15 13:36:35 by julmuntz         ###   ########.fr       */
+/*   Updated: 2023/02/15 18:01:55 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int	start(char **arg, int n)
+{
+	int	i;
+	int	c;
+
+	i = n;
+	while (arg[i] && !ft_strncmp(arg[i], "-n", 2))
+	{
+		c = 2;
+		while (arg[i][c] == 'n')
+			c++;
+		if (!arg[i][c])
+			i++;
+		else
+			break ;
+	}
+	return (i);
+}
+
 void	cmd_echo(t_data *d, t_lst *l)
 {
 	int	i;
+	int	n;
 
-	i = 1;
-	if (!ft_strcmp(l->arg[1], "-n"))
-		i = 2;
 	if (!l->arg[1])
-		ft_fprintf(d->out, "\n");
-	else if (l->arg[1])
+		return ((void)write(d->out, "\n", 1));
+	i = 1;
+	n = 1;
+	i = start(l->arg, n);
+	if (l->arg[1])
 	{
 		while (l->arg[i])
 		{
 			ft_fprintf(d->out, "%s", l->arg[i]);
 			if ((i) == (int)ft_arrstrlen(l->arg) - 1)
 				break ;
-			ft_fprintf(d->out, " ");
+			write(d->out, " ", 1);
 			i++;
+			n++;
 		}
-		if (ft_strcmp(l->arg[1], "-n"))
-			ft_fprintf(d->out, "\n");
+		if (n == i)
+			write(d->out, "\n", 1);
 	}
 }
